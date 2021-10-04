6cc1a30b58f87df02783e51ed54bc62b53f000b4
Lucas Rosa <lucasmetron@gmail.com>
2021-10-04 08:45:10 -0300

    progressbar android

--- App.js
+++ App.js
@@ -1,30 +1,46 @@
 import React, { useEffect } from 'react';
 import {
   SafeAreaView,
-  StatusBar,
   StyleSheet,
   Text,
   View,
+  Platform,
+  Progress
 
 } from 'react-native';
+
+import { ProgressBar } from '@react-native-community/progress-bar-android';
 
 
 const App = () => {
 
   useEffect(() => {
-    StatusBar.setBackgroundColor('red', true); //primerio parâmetro cor e segundo se é ou não animado
-    StatusBar.setBarStyle('light-content', true); //primerio parâmetro cor e segundo se é ou não animado
-    StatusBar.setHidden(false, true) //primerio parâmetro se é esconde ou nao e segundo se é ou não animado
+
   }, [])
 
   return (
     <SafeAreaView>
       <View>
-        <StatusBar
-          backgroundColor='lightblue'
-          barStyle='dark-content'
-          hidden={true}
-        />
+        <View style={styles.example}>
+          <Text>Circle Progress Indicator</Text>
+          <ProgressBar />
+        </View>
+        <View style={styles.example}>
+          <Text>Horizontal Progress Indicator</Text>
+          <ProgressBar styleAttr="Horizontal" />
+        </View>
+        <View style={styles.example}>
+          <Text>Colored Progress Indicator</Text>
+          <ProgressBar styleAttr="Horizontal" color="#2196F3" />
+        </View>
+        <View style={styles.example}>
+          <Text>Fixed Progress Value</Text>
+          <ProgressBar
+            styleAttr="Horizontal"
+            indeterminate={false}
+            progress={0.5}
+          />
+        </View>
         <Text>Teste</Text>
       </View>
     </SafeAreaView>
@@ -32,21 +48,13 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  example: {
+    marginVertical: 24,
   },
 });
 
