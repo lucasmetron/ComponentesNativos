d6ec693fa300d15c078da34fe4a264b95d59d601
Lucas Rosa <lucasmetron@gmail.com>
2021-10-04 16:13:38 -0300

    toolbarandroid limpo

--- App.js
+++ App.js
@@ -26,29 +26,22 @@ const App = () => {
       <View>
         <ToolbarAndroid
           title="Meu App"
-          titleColor='white'
           subtitle="Desc. App"
-          subtitleColor='red'
-          // navIcon={icon} icone lado esquerdo
-          actions={[ //ações que seram feitas ao clicar nelas
+          actions={[
             {
               title: 'Configurações',
-              icon: icon,
             },
             {
               title: 'Opções',
-              show: 'always', //mostra botão na tela, se não tiver o always todas actions ficam em um menu modal
-              icon: icon, //icone da action
             },
           ]}
           style={{
             backgroundColor: '#8196F3',
             height: 56
           }}
-          onActionSelected={handleAction} //função disparada ao selecionar action
-          onIConClicked={handleAction} //funcção disparada ao sleecionar menu principal 
+          onActionSelected={handleAction}
+          onIConClicked={handleAction}
         />
-        <Text>teste</Text>
       </View>
     </SafeAreaView>
   );
